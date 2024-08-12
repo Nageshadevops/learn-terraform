96e7d720be4518aaa44a249eb1e442e70df57c79
nagesha technology <asbconstruction19@gmail.com>
2024-08-12 12:09:13 +0530

    tag =tags

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -2,16 +2,15 @@ resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0989be542cc7aee19"]
-  tag = {
+  tags = {
     Name = "frontend"
   }
 }
-
 resource "aws_instance" "backend" {
   ami           = "ami-041e2ea9402c46c32"
   vpc_security_group_ids = ["sg-0989be542cc7aee19"]
   instance_type = "t3.micro"
-  tag = {
+  tags = {
     Name = "backend"
   }
 }
@@ -20,7 +19,7 @@ resource "aws_instance" "mysql" {
   ami           = "ami-041e2ea9402c46c32"
   vpc_security_group_ids = ["sg-0989be542cc7aee19"]
   instance_type = "t3.micro"
-  tag = {
+  tags = {
     Name = "mysql"
   }
 }
